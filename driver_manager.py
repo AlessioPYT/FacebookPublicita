201aa0f41079ef1ffe8a1425df0ebb78b2d5c544
Alessio <165054717+AlessioPYT@users.noreply.github.com>
2024-07-12 00:17:31 +0300

    Update driver_manager.py

--- driver_manager.py
+++ driver_manager.py
@@ -2,9 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Driver():
@@ -12,15 +10,18 @@ class Driver():
 
     @classmethod
     def start(cls):
-        chrome_options = Options()
-        # chrome_options.add_argument("--headless")   
-        chrome_options.add_argument("user-data-dir=C:\\Users\\Алексей\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1")          
-        cls.driver = webdriver.Chrome(options=chrome_options)
+        options = Options()
+        options.add_argument("dom.webnotifications.enabled")
+        prefs = {"profile.default_content_setting_values.notifications": 2}  # Отключить уведомления
+        options.add_experimental_option("prefs", prefs)
+        # chrome_options.add_argument("--headless")   # фоновий режим
+        options.add_argument("user-data-dir=C:\\Users\\Алексей\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1") # свой профиль          
+        cls.driver = webdriver.Chrome(options=options)
         return cls.driver
     
     @classmethod
     def open_browser(cls):
-        cls.driver.get("https://ua.scryde.net/panel/?character=273205599&server=x100&action=activate")
+        cls.driver.get("https://www.facebook.com/groups/smilesofworld")
         WebDriverWait(cls.driver, 10).until(EC.presence_of_element_located((By.ID, "email")))                        
         return cls.driver    
         

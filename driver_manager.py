6b96e52f2464b093d83d3f69179f12e5decdf44a
Alessio <165054717+AlessioPYT@users.noreply.github.com>
2024-07-17 14:49:12 +0300

    Update driver_manager.py

--- driver_manager.py
+++ driver_manager.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='test_log.log')
 
 class Driver():
     driver = None
 
     @classmethod
     def start(cls):
+        logging.info("Start Browser")
         options = Options()
         options.add_argument("dom.webnotifications.enabled")
         prefs = {"profile.default_content_setting_values.notifications": 2}  # Отключить уведомления
@@ -21,13 +24,15 @@ class Driver():
     
     @classmethod
     def open_browser(cls):
-        cls.driver.get("https://www.facebook.com/groups/smilesofworld")
-                                
+        logging.info("Opening browser from driver")
+        cls.driver.get("https://www.facebook.com/")
+        # WebDriverWait(cls.driver, 10).until(EC.presence_of_element_located((By.ID, "email")))                        
         return cls.driver    
         
     @classmethod                                        
     def finish(cls):
         if cls.driver:
+            logging.info("Close driver")
             cls.driver.quit()
 
 
